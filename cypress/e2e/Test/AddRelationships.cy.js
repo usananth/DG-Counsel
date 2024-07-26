3a2f77c00052d7521276dccb73a55332f1fa9659
us.ananth <us.ananthakumar@gmail.com>
2024-07-26 22:32:44 +0530

    SourceChange

--- cypress/e2e/Test/AddRelationships.cy.js
+++ cypress/e2e/Test/AddRelationships.cy.js
@@ -1,51 +1,51 @@
-import RelationshipPage from "../../PageObjects/RelationshipPage";
-import Login from "../../PageObjects/LoginPage";
+import RelationshipPage from '../../PageObjects/RelationshipPage'
+import Login from '../../PageObjects/LoginPage'
 
-describe(" Add Relationships Module", () => {
-  Cypress.on("uncaught:exception", (err, runnable) => {
-    return false;
-  });
-  var data, input, updateDepts;
+describe(' Add Relationships Module', () => {
+	Cypress.on('uncaught:exception', (err, runnable) => {
+		return false
+	})
+	var data, input, updateDepts
 
-  before(() => {
-    Login.visit();
-    cy.fixture("Relationships").then((value) => {
-      data = value;
-      Login.userName(data.loginData.inputmail);
-      Login.userPassword(data.loginData.inputpassword);
-      Login.submitBtn();
-      input = data.searchFirm.inputSearch;
-      //[data.selectDepartments.Dept1, data.selectDepartments.Dept2];
-    });
-  });
-  beforeEach(() => {
-    updateDepts = [data.updateDepts.Dept1, data.updateDepts.Dept2];
-    RelationshipPage.RelationshipMenu();
-    RelationshipPage.AddRelationshipTabs();
-  });
+	before(() => {
+		Login.visit()
+		cy.fixture('Relationships').then(value => {
+			data = value
+			Login.userName(data.loginData.inputmail)
+			Login.userPassword(data.loginData.inputpassword)
+			Login.submitBtn()
+			input = data.searchFirm.inputSearch
+			//[data.selectDepartments.Dept1, data.selectDepartments.Dept2];
+		})
+	})
+	beforeEach(() => {
+		updateDepts = [data.updateDepts.Dept1, data.updateDepts.Dept2]
+		RelationshipPage.RelationshipMenu()
+		RelationshipPage.AddRelationshipTabs()
+	})
 
-  it.only("TestCase: 1.ViewRelationshipTab-UpdateDeptAccess-AlertYes", () => {
-    RelationshipPage.SearchbarInput(input);
-    RelationshipPage.UpdateDepts(updateDepts);
-    RelationshipPage.UpdateDeptSaveBtn();
-    RelationshipPage.AlertYesBtn();
-    RelationshipPage.AssertDepts(data.assertDepts.deptOne);
-    RelationshipPage.AssertDepts(data.assertDepts.deptTwo);
-    RelationshipPage.SuccessPopupXBtn();
-  });
-  it("TestCase: 2.ViewRelationshipTab-UpdateDeptAccess-AlertNo", () => {
-    RelationshipPage.SearchbarInput1(input);
-    RelationshipPage.UpdateDepts(updateDepts);
-    RelationshipPage.UpdateDeptSaveBtn();
-    RelationshipPage.AlertNoBtn();
-    RelationshipPage.UpdateDeptCancelBtn();
-  });
+	it.only('TestCase: 1.ViewRelationshipTab-UpdateDeptAccess-AlertYes', () => {
+		RelationshipPage.SearchbarInput(input)
+		RelationshipPage.UpdateDepts(updateDepts)
+		RelationshipPage.UpdateDeptSaveBtn()
+		RelationshipPage.AlertYesBtn()
+		RelationshipPage.AssertDepts(data.assertDepts.deptOne)
+		RelationshipPage.AssertDepts(data.assertDepts.deptTwo)
+		RelationshipPage.SuccessPopupXBtn()
+	})
+	it('TestCase: 2.ViewRelationshipTab-UpdateDeptAccess-AlertNo', () => {
+		RelationshipPage.SearchbarInput1(input)
+		RelationshipPage.UpdateDepts(updateDepts)
+		RelationshipPage.UpdateDeptSaveBtn()
+		RelationshipPage.AlertNoBtn()
+		RelationshipPage.UpdateDeptCancelBtn()
+	})
 
-  it("TestCase: 3.ViewRelationshipTab-UpdateDeptAccess-YesPopupClose", () => {
-    RelationshipPage.SearchbarInput1(input);
-    RelationshipPage.UpdateDepts(updateDepts);
-    RelationshipPage.UpdateDeptSaveBtn();
-    RelationshipPage.AlertYesBtn();
-    RelationshipPage.SuccessPopupXBtn();
-  });
-});
+	it('TestCase: 3.ViewRelationshipTab-UpdateDeptAccess-YesPopupClose', () => {
+		RelationshipPage.SearchbarInput1(input)
+		RelationshipPage.UpdateDepts(updateDepts)
+		RelationshipPage.UpdateDeptSaveBtn()
+		RelationshipPage.AlertYesBtn()
+		RelationshipPage.SuccessPopupXBtn()
+	})
+})
